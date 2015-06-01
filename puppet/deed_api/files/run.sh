a081b5a0ceb4e0ac4d10fa3aa71af8b20d29429f
Michael Allen <michael@michaelallen.io>
2015-06-01 15:50:56 +0100

    rename the puppet module to reflect the deed_api

---
+++ puppet/deed_api/files/run.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+virtualenv ./.service_env
+
+source ./.service_env/bin/activate
+
+pip3 install -r requirements.txt
+
+port="${DEED_API_GUNICORN_PORT:-8000}"
+host="${DEED_API_GUNICORN_HOST:-0.0.0.0}"
+
+
+gunicorn -b $host:$port --pid /var/run/deed_api.pid "app:create_manager().app"
--- puppet/deed_api/manifests/init.pp
+++ puppet/deed_api/manifests/init.pp
@@ -1,51 +1,51 @@
 # Install and configure the Flask Api Skeleton
-class api_skeleton (
-    $port = '9010',
+class deed_api (
+    $port = '9012',
     $host = '0.0.0.0',
     $branch_or_revision = 'master'
 ) {
   require ::standard_env
 
-  vcsrepo { '/opt/api_skeleton':
+  vcsrepo { '/opt/deed_api':
     ensure   => latest,
     provider => git,
-    source   => 'git://github.com/LandRegistry/charges-api-skeleton',
+    source   => 'git://github.com/LandRegistry/charges-deed-api',
     revision => $branch_or_revision,
     owner    => 'vagrant',
     group    => 'vagrant',
-    notify   => Service['api_skeleton'],
+    notify   => Service['deed_api'],
   }
 
-  file { '/opt/api_skeleton/bin/run.sh':
+  file { '/opt/deed_api/bin/run.sh':
     ensure  => 'file',
     mode    => '0755',
     owner   => 'vagrant',
     group   => 'vagrant',
     source  => "puppet:///modules/${module_name}/run.sh",
-    require => Vcsrepo['/opt/api_skeleton'],
-    notify  => Service['api_skeleton'],
+    require => Vcsrepo['/opt/deed_api'],
+    notify  => Service['deed_api'],
   }
 
-  file { '/etc/systemd/system/api_skeleton.service':
+  file { '/etc/systemd/system/deed_api.service':
     ensure  => 'file',
     mode    => '0755',
     owner   => 'vagrant',
     group   => 'vagrant',
     content => template("${module_name}/service.systemd.erb"),
-    notify  => [Exec['systemctl-daemon-reload'], Service['api_skeleton']],
+    notify  => [Exec['systemctl-daemon-reload'], Service['deed_api']],
   }
-  service { 'api_skeleton':
+  service { 'deed_api':
     ensure   => 'running',
     enable   => true,
     provider => 'systemd',
     require  => [
-      Vcsrepo['/opt/api_skeleton'],
-      File['/opt/api_skeleton/bin/run.sh'],
-      File['/etc/systemd/system/api_skeleton.service']
+      Vcsrepo['/opt/deed_api'],
+      File['/opt/deed_api/bin/run.sh'],
+      File['/etc/systemd/system/deed_api.service']
     ],
   }
 
-  file { '/etc/nginx/conf.d/api_skeleton.conf':
+  file { '/etc/nginx/conf.d/deed_api.conf':
     ensure  => 'file',
     mode    => '0755',
     content => template("${module_name}/nginx.conf.erb"),
