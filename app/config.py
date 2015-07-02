f779cf02659c6f71106cd2947dfa7844990b9052
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2015-07-02 10:53:05 +0100

    removed redundent try catch

--- app/config.py
+++ app/config.py
@@ -2,8 +2,4 @@ import os
 
 DEBUG = True
 
-try:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DEED_DATABASE_URI', '')
-except KeyError:
-    print("[ERROR] You need set the export variable for DEED_DATABASE_URI")
-    raise
+SQLALCHEMY_DATABASE_URI = os.getenv('DEED_DATABASE_URI', '')
