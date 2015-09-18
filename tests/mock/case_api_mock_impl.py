f4aec0d432e4d656e0dd84d46a399cff8d96d4bf
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-09-18 10:40:24 +0100

    mock response class added

--- tests/mock/case_api_mock_impl.py
+++ tests/mock/case_api_mock_impl.py
@@ -1,15 +1,14 @@
 from flask import jsonify
 from flask.ext.api import status
+from .mock_response import MockResponse
 
 
 class MockCaseApi(object):
-
     def update_status(self, deed_id, case_status):
         return jsonify(status_code=status.HTTP_200_OK)
 
     def get_borrowers(self, case_id):
-
-        return jsonify({"borrowers": [
+        return MockResponse([
             {
                 "id": "1",
                 "name": "John Smith",
@@ -20,11 +19,10 @@ class MockCaseApi(object):
                     "extended-address": "test-extended address"
                 }
             }
-        ]})
+        ])
 
     def get_property(self, case_id):
-
-        return jsonify({"title": {
+        return MockResponse({"title_number": {
             "address": {
                 "street-address": "test street",
                 "postal-code": "RG1 1DP",
