e4f919e6d3019383980a5e2067155c817efff2cb
tsangsimon <simon.tsang@gmail.com>
2015-09-18 12:13:26 +0100

    fixing more linting issues

--- tests/mock/case_api_mock_impl.py
+++ tests/mock/case_api_mock_impl.py
@@ -1,5 +1,4 @@
-import json
-from flask import jsonify, Response
+from flask import jsonify
 from flask.ext.api import status
 from .mock_response import MockResponse
 
@@ -10,23 +9,23 @@ class MockCaseApi(object):
 
     def get_borrowers(self, case_id):
         return MockResponse([{"last_name": "ggtg",
-                "id": "25",
-                "mobile_no": "09494309393",
-                "type": "Borrower",
-                "email_address": "a@b.com",
-                "first_name": "rt",
-                "middle_names": "",
-                "address": ["2 rap street", "", "faketown", "F4K3"],
-                "case_id": 16}])
-                
+                              "id": "25",
+                              "mobile_no": "09494309393",
+                              "type": "Borrower",
+                              "email_address": "a@b.com",
+                              "first_name": "rt",
+                              "middle_names": "",
+                              "address": ["2 rap street", "",
+                                          "faketown", "F4K3"],
+                              "case_id": 16}])
 
     def get_property(self, case_id):
         return MockResponse({"title_number": "LO3827",
-                            "id": 9,
-                            "extended": "Market Square",
-                            "street": "42A Broad Street",
-                            "type": "Property",
-                            "locality": "Slough",
-                            "case_id": 19,
-                            "tenure": "freehold",
-                            "postcode": "SL2 1TP"})
+                             "id": 9,
+                             "extended": "Market Square",
+                             "street": "42A Broad Street",
+                             "type": "Property",
+                             "locality": "Slough",
+                             "case_id": 19,
+                             "tenure": "freehold",
+                             "postcode": "SL2 1TP"})
