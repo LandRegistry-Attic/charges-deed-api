4a15b6f32b39320c10573342c31790f97fc1ba70
tsangsimon <simon.tsang@gmail.com>
2015-10-07 10:27:54 +0100

    resolving path issues

--- acceptance-tests/features/steps/create_data.rb
+++ acceptance-tests/features/steps/create_data.rb
@@ -1,14 +1,13 @@
 Given(/^I have created a case and deed with one borrower that is effective$/) do
   @created_case_id = create_case_data
 
-  puts Dir.pwd
-  borrower_json = File.read('./features/data/one_borrower.json')
+  borrower_json = File.read('./acceptance-tests/features/data/one_borrower.json')
   @added_borrowers = add_borrowers_to_case(@created_case_id, borrower_json)
 
-  property_json = File.read('./features/data/mortgage_property.json')
+  property_json = File.read('./acceptance-tests/features/data/mortgage_property.json')
   @added_property = add_property_to_case(@created_case_id, property_json)
 
-  deed_json = File.read('./features/data/create_deed.json')
+  deed_json = File.read('./acceptance-tests/features/data/create_deed.json')
   @created_deed_id = create_deed_data(@created_case_id, deed_json)
   ### Link the created deed and case together
   update_case_deed(@created_deed_id, @created_case_id)
@@ -26,13 +25,13 @@ end
 Given(/^I have created a case and deed with two borrowers$/) do
   @created_case_id = create_case_data
 
-  borrower_json = File.read('./features/data/two_borrowers.json')
+  borrower_json = File.read('./acceptance-tests/features/data/two_borrowers.json')
   @added_borrowers = add_borrowers_to_case(@created_case_id, borrower_json)
 
-  property_json = File.read('./features/data/mortgage_property.json')
+  property_json = File.read('./acceptance-tests/features/data/mortgage_property.json')
   @added_property = add_property_to_case(@created_case_id, property_json)
 
-  deed_json = File.read('./features/data/create_deed.json')
+  deed_json = File.read('./acceptance-tests/features/data/create_deed.json')
   @created_deed_id = create_deed_data(@created_case_id, deed_json)
   ### Link the created deed and case together
   update_case_deed(@created_deed_id, @created_case_id)
